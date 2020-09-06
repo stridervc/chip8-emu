d941ff847ce3d5c68912b73036b08015adc69e06
Fred Strauss <fred@strider.co.za>
2020-09-06 14:26:26 +0200

    More work on implementing instructions

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ import Chip8Instructions
 
 main :: IO ()
 main = do
-  let Just c = ld chip8 12 0xff
+  let Just c = instrLd chip8 12 0xff
 
   putStrLn $ "Vc = " ++ (show $ getVReg chip8 12)
   putStrLn $ show $ vregs chip8
--- src/Chip8Instructions.hs
+++ src/Chip8Instructions.hs
@@ -1,81 +1,116 @@
 module Chip8Instructions
-  ( cls
-  , ret
-  , jmp
-  , call
-  , se
-  , sne
-  , ser
-  , ld
+  ( instrCls
+  , instrRet
+  , instrJmp
+  , instrCall
+  , instrSe
+  , instrSne
+  , instrSeR
+  , instrLd
   ) where
 
 import Chip8
+import Data.Bits
 
 -- increment program counter
 -- this should always be an even numbers since opcodes
 -- are 16-bit
-incpc :: Chip8 -> Maybe Chip8
-incpc c = return $ c { pc = pc c + 2 }
+incpc :: Chip8 -> Chip8
+incpc c = c { pc = pc c + 2 }
 
 -- 00E0 clear screen
-cls :: Chip8 -> Maybe Chip8
-cls c = incpc $ c { screen = take (w*h) $ repeat False }
+instrCls :: Chip8 -> Maybe Chip8
+instrCls c = return $ incpc $
+  c { screen = take (w*h) $ repeat False }
   where w = fst $ screensize c
         h = snd $ screensize c
 
 -- 00EE return
-ret :: Chip8 -> Maybe Chip8
-ret c = do
+instrRet :: Chip8 -> Maybe Chip8
+instrRet c = do
   (c', a) <- pop c
-  jmp c' a
+  instrJmp c' a
 
 -- 1nnn jump nnn
-jmp :: Chip8 -> Address -> Maybe Chip8
-jmp c a = return $ c { pc = a }
+instrJmp :: Chip8 -> Address -> Maybe Chip8
+instrJmp c a = return $ c { pc = a }
 
 -- 2nnn call nnn
-call :: Chip8 -> Address -> Maybe Chip8
-call c a = do
-  c' <- incpc c
-  jmp c' a
+instrCall :: Chip8 -> Address -> Maybe Chip8
+instrCall c a = instrJmp (incpc c) a
 
 -- 3xkk SE Vx, byte
 -- skip next instruction if Vx == kk
-se :: Chip8 -> Register -> Byte -> Maybe Chip8
-se c r v
-  | equal     = do
-    c' <- incpc c
-    incpc c'
-  | otherwise = incpc c
+instrSe :: Chip8 -> Register -> Byte -> Maybe Chip8
+instrSe c r v
+  | equal     = return $ incpc $ incpc c
+  | otherwise = return $ incpc c
   where equal = vx == v
         vx    = getVReg c r
 
 -- 4xkk SNE Vx, byte
 -- skip next instruction if Vx /= kk
-sne :: Chip8 -> Register -> Byte -> Maybe Chip8
-sne c r v
-  | equal     = incpc c
-  | otherwise = do
-    c' <- incpc c
-    incpc c'
+instrSne :: Chip8 -> Register -> Byte -> Maybe Chip8
+instrSne c r v
+  | equal     = return $ incpc c
+  | otherwise = return $ incpc $ incpc c
   where equal = vx == v
         vx    = getVReg c r
 
 -- 5xy0 SE Vx, Vy
 -- skip next instruction if Vx == Vy
-ser :: Chip8 -> Register -> Register -> Maybe Chip8
-ser c a b
-  | equal     = do
-    c' <- incpc c
-    incpc c'
-  | otherwise = incpc c
+instrSeR :: Chip8 -> Register -> Register -> Maybe Chip8
+instrSeR c a b
+  | equal     = return $ incpc $ incpc c
+  | otherwise = return $ incpc c
   where equal = va == vb
         va    = getVReg c a
         vb    = getVReg c b
 
 -- 6xkk LD Vx, byte
 -- set register x to value kk
-ld :: Chip8 -> Register -> Byte -> Maybe Chip8
-ld c r v = do
-  c' <- setVReg c r v
-  incpc c'
+instrLd :: Chip8 -> Register -> Byte -> Maybe Chip8
+instrLd c r v = incpc <$> setVReg c r v
+
+-- 7xkk ADD Vx, byte
+-- Vx = Vx + kk
+instrAdd :: Chip8 -> Register -> Byte -> Maybe Chip8
+instrAdd c r v = incpc <$> setVReg c r (vx+v)
+  where vx  = getVReg c r
+
+-- 8xy0 LD Vx, Vy
+-- set Vx to Vy's value
+instrLdx :: Chip8 -> Register -> Register -> Maybe Chip8
+instrLdx c x y = incpc <$> setVReg c x vy
+  where vy  = getVReg c y
+
+-- 8xy1 OR Vx, Vy
+-- Vx = Vx OR Vy
+instrOr :: Chip8 -> Register -> Register -> Maybe Chip8
+instrOr c x y = incpc <$> setVReg c x (vx .|. vy)
+  where vx  = getVReg c x
+        vy  = getVReg c y
+
+-- 8xy2 AND Vx, Vy
+-- Vx = Vx AND Vy
+instrAnd :: Chip8 -> Register -> Register -> Maybe Chip8
+instrAnd c x y = incpc <$> setVReg c x (vx .&. vy)
+  where vx  = getVReg c x
+        vy  = getVReg c y
+
+-- 8xy3 XOR Vx, Vy
+-- Vx = Vx XOR Vy
+instrXor :: Chip8 -> Register -> Register -> Maybe Chip8
+instrXor c x y = incpc <$> setVReg c x (vx `xor` vy)
+  where vx  = getVReg c x
+        vy  = getVReg c y
+
+-- 8xy4 ADD Vx, Vy
+-- Vx = Vx + Vy
+instrAddR :: Chip8 -> Register -> Register -> Maybe Chip8
+instrAddR c x y = do
+  c' <- setVReg c 15 vf
+  incpc <$> setVReg c' x (vx+vy)
+  where vx  = getVReg c x
+        vy  = getVReg c y
+        vf  = if (toInteger vx + toInteger vy) > 255 then 1 else 0
