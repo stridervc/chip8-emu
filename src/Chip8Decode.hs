5201a121589de96ecb89202f08da686d64996688
Fred Strauss <fred@strider.co.za>
2020-09-18 11:58:58 +0200

    Decode some more instructions

--- src/Chip8Decode.hs
+++ src/Chip8Decode.hs
@@ -19,9 +19,16 @@ decodeOpcode opcode
   | hinib == 0x4      = "sne v" <> hex x <+> hex kk
   | hinib == 0x5      = "se v" <> hex x <+> "v" <> hex y
   | hinib == 0x6      = "mov v" <> hex x <+> hex kk
+  | hinib == 0x7      = "add v" <> hex x <+> hex kk
+  | hinib == 0x9      = "sne v" <> hex x <+> "v" <> hex y
+  | hinib == 0xa      = "mov i" <+> hex nnn
+  | hinib == 0xb      = "jmp v0+" <> hex nnn
+  | hinib == 0xc      = "rnd v" <> hex x <+> hex kk
+  | hinib == 0xd      = "drw v" <> hex x <+> "v" <> hex y <+> hex n
 
   where hinib   = opcode `shiftR` 12
         nnn     = opcode .&. 0x0fff
+        n       = fromIntegral $ opcode .&. 0x000f
         x       = fromIntegral $ (opcode .&. 0x0f00) `shiftR` 8
         y       = fromIntegral $ (opcode .&. 0x00f0) `shiftR` 4
         kk      = fromIntegral $ opcode .&. 0x00ff
